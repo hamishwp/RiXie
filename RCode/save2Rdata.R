8d64a43fa2478abbe088dacabe2d6f77eec5493a
coleencarranza <cducarranza@yahoo.com>
2022-11-01 14:25:14 +0100

    spotlight table

--- RCode/save2Rdata.R
+++ RCode/save2Rdata.R
@@ -1,7 +1,7 @@
 ###---------------Generate RData for some codes and lists--------------
 
 ##-------------------description for INFORM index or indicator codes----------
-Inform_codes_desc <- read.csv("/home/coleen/Documents/GitHub/GRAF_files/INFORM_codes_list.csv",header = TRUE)[,4:5]
+Inform_codes_desc <- read.csv("/home/coleen/Documents/GRAF_files/INFORM_codes_list.csv",header = TRUE)[,4:5]
 
 
 ##--------------------description WorldBank indicators---------------
@@ -12,10 +12,10 @@ Wbank_codes_desc<-data.frame(wbstats::wb_indicators())
 
 
 #----------------shapefile adm labels---------------------------------------
-#adm_labels <-read.csv("/home/coleen/Documents/GitHub/RiXie/Data/Spatial/Field_Names.csv",header=T)
+adm_labels <-read.csv("/home/coleen/Documents/Data/Spatial/Field_Names.csv",header=T)
 
 #--------------Adm sources-------------------------------------------------
-#adm_sources <-read.csv("/home/coleen/Documents/GitHub/RiXie/Data/Spatial/MappingDataSources.csv",header=T)
+adm_sources <-read.csv("/home/coleen/Documents/Data/Spatial/MappingDataSources.csv",header=T)
 
 
 save.image(file="codes_desc.RData")
