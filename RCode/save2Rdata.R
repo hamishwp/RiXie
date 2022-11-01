7405ac521b915aebe91fc5c22d0f3bacf2ec11cf
coleencarranza <cducarranza@yahoo.com>
2022-11-01 10:42:21 +0100

    spotlight tables and map modified

--- RCode/save2Rdata.R
+++ RCode/save2Rdata.R
@@ -1,10 +1,5 @@
 ###---------------Generate RData for some codes and lists--------------
 
-##-----------------ISO country codes---------
-iso3 <-read.csv("/home/coleen/Documents/GitHub/ISO-3166-Countries-with-Regional-Codes/slim-3/slim-3.csv", header=TRUE)
-colnames(iso3) <- c("Country","iso3","code")
-
-  
 ##-------------------description for INFORM index or indicator codes----------
 Inform_codes_desc <- read.csv("/home/coleen/Documents/GitHub/GRAF_files/INFORM_codes_list.csv",header = TRUE)[,4:5]
 
